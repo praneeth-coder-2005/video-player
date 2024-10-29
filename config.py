e43f5e726f0b4cf2a900803b3696acecb1852c38
praneeth-coder-2005 <savediskofc@gmail.com>
2024-10-29 10:48:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,3 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-GOOGLE_CREDENTIALS = os.getenv('GOOGLE_CREDENTIALS')
-DRIVE_FOLDER_ID = os.getenv('DRIVE_FOLDER_ID')
