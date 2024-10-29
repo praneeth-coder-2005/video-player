46a2fa0c4d8491036ce26fd6523b23351a124083
praneeth-coder-2005 <savediskofc@gmail.com>
2024-10-29 10:45:43 +0530

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,8 @@
+import requests
+
+def download_file(url, dest_path):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    with open(dest_path, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
