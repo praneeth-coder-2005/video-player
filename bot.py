c62352ce7aa71fd70b5df9a396c074278485527f
praneeth-coder-2005 <savediskofc@gmail.com>
2024-10-30 14:18:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -90,4 +90,4 @@ def webhook():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Assign port for deployment
-     app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
